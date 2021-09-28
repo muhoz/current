7ab0b4edc9ba7b72b188660428f9cb350e513b4e
muhoz <71699176+muhoz@users.noreply.github.com>
2021-09-28 15:17:02 +0100

    Update code_courant.ino

--- code_courant.ino
+++ code_courant.ino
@@ -1,31 +1,31 @@
-#include <Filters.h>
+#include <Filters.h>  //external library that can be downloaded at this adress: https://www.arduinolibraries.info/libraries/filters
 
-float cour;
+//declare the variable
 float cour1 = 0;
 
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(A7,INPUT);
+  Serial.begin(9600);  //initialise the serial communication
+  pinMode(A7,INPUT);   //Define the port as an INPUT to read the current value at this position
 }
 
 void loop(){
-    cour1 = courant_1();
-Serial.println(cour1);
+    cour1 = courant_1();  //call the courant_1() function that calculates the current
+Serial.println(cour1);    //print the current value
 }
 
-
+//current function
 float courant_1() {
   unsigned int x = 0;
   float AcsValue = 0.0, Samples = 0.0, AvgAcs = 0.0, AcsValueF = 0.0;
 
-  for (int x = 0; x < 150; x++) { 
-    AcsValue = analogRead(A7);     
-    Samples = Samples + AcsValue;  
-    delay (3);
+  for (int x = 0; x < 150; x++) { //loop to take 150 samples to do the mean and have a better value
+    AcsValue = analogRead(A7);     //read the value from the analog pin
+    Samples = Samples + AcsValue;  //sum the reading values
+    delay (3);  //wait 3 milliseconds
   }
-  AvgAcs = Samples / 150.0; 
-  AcsValueF = ((AvgAcs * (5.0 / 1024.0))) / 0.66;
-  delay(50);
-  return abs(AcsValueF);
+  AvgAcs = Samples / 150.0; //divide to have the arithmetic mean
+  AcsValueF = ((AvgAcs * (5.0 / 1024.0))) / 0.66;  //conversion to have the real value of the current sensor
+  delay(50);  //wait 50 milliseconds
+  return abs(AcsValueF); //return the current value
 }
